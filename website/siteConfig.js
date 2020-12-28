d331a1aa81fe73e80a55e3987deef4c4f98a63a2
chipyaya <b03902024@ntu.edu.tw>
2020-12-28 13:26:27 -0600

    update profile photo

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [];
 const siteConfig = {
     title: 'Hsiao-Hua (Evelyn) Cheng', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
-    profilePhoto: 'img/profile.png',
+    profilePhoto: 'img/profile.jpg',
     url: 'https://chipyaya.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
